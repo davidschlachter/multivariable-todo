de1ea3228efb8339d93ccf5f02f7437f29013a90
David Schlachter <david+gitlab@schlachter.ca>
2015-10-17 12:31:04 -0400

    Calculate priority number, fetch tasks from server in dedicated function

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,31 +2,7 @@
 "use strict";
 $(document).ready(function () {
 
-	$.ajax({
-		url: 'getTasks',
-		type: 'POST',
-		data: {
-			gettasks: "get tasks"
-		},
-		success: function (result) {
-			if (result) {
-				var len = result.length;
-				var txt = "";
-				if (len > 0) {
-					for (var i = 0; i < len; i++) {
-						if (result[i].coursecode && result[i].task && result[i].deadline && result[i].weight) {
-							txt += "<tr><td>" + result[i].coursecode + "</td><td>" + result[i].task + "</td><td>" + result[i].deadline + "</td><td>" + result[i].weight + "</td></tr>";
-						}
-					}
-					if (txt != "") {
-						$("#tasksTable").find("tr:gt(0)").remove();
-						$("#tasksTable").append(txt);
-					}
-				}
-			}
-		}
-	});
-
+	GetTasks();
 
 	$('#inputDeadline').datetimepicker({
 		timeFormat: 'hh:mm tt z'
@@ -45,25 +21,49 @@ $(document).ready(function () {
 				'weight': $('#inputWeight').val()
 			},
 			success: function (result) {
-				console.log(result);
-
-				if (result) {
-					var len = result.length;
-					var txt = "";
-					if (len > 0) {
-						for (var i = 0; i < len; i++) {
-							if (result[i].coursecode && result[i].task && result[i].deadline && result[i].weight) {
-								txt += "<tr><td>" + result[i].coursecode + "</td><td>" + result[i].task + "</td><td>" + result[i].deadline + "</td><td>" + result[i].weight + "</td></tr>";
-							}
-						}
-						if (txt != "") {
-							$("tasksTable").find("tr:gt(0)").remove();
-							$("#tasksTable").append(txt);
-						}
-					}
-				}
+				GetTasks();
 			}
 		});
 
 	});
 });
+
+function JSDateToExcelDate(inDate) {
+	var returnDateTime = 25569.0 + ((inDate.getTime() - (inDate.getTimezoneOffset() * 60 * 1000)) / (1000 * 60 * 60 * 24));
+	return returnDateTime.toString().substr(0, 20);
+}
+
+function GetTasks() {
+	$.ajax({
+		url: 'getTasks',
+		type: 'POST',
+		data: {
+			gettasks: "get tasks"
+		},
+		success: function (result) {
+			if (result) {
+				var len = result.length;
+				var txt = "";
+				var priority;
+				var rightNow = new Date();
+				if (len > 0) {
+					for (var i = 0; i < len; i++) {
+						if (result[i].coursecode && result[i].task && result[i].deadline && result[i].weight) {
+							result[i].deadline = new Date(result[i].deadline);
+							priority = (result[i].weight / (JSDateToExcelDate(result[i].deadline) - JSDateToExcelDate(rightNow))) * 100;
+							priority = parseFloat(priority).toFixed(2);
+							if (priority < 0) {
+								continue;
+							}
+							txt += "<tr><td>" + result[i].coursecode + "</td><td>" + result[i].task + "</td><td>" + result[i].deadline + "</td><td>" + result[i].weight + "</td><td class=\"priority\">" + priority + "</td></tr>";
+						}
+					}
+					if (txt != "") {
+						$("#tasksTable").find("tr:gt(0)").remove();
+						$("#tasksTable").append(txt);
+					}
+				}
+			}
+		}
+	});
+}
