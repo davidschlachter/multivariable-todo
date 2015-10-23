2e164a48b21b21f5efe107865e75ee6c9a1b45b7
David Schlachter <david+gitlab@schlachter.ca>
2015-10-22 20:33:17 -0400

    Update the table every five minutes, poll the server for updates every half hour

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,21 @@
 
 "use strict";
-$(document).ready(function () {
 
-	if ($("#tasksTable").length) {
-		GetTasks();
-		sortTable();
-	}
+var todosList;
+var oldList;
+var sleepycounter = 0;
+
+$(document).ready(function () {
 
 	if ($('#inputDeadline').length) {
 		$('#inputDeadline').datetimepicker({
 			timeFormat: 'hh:mm tt z'
 		});
+	}
+
+	if ($("#tasksTable").length) {
+		GetTasks();
+		sortTable();
 	}
 
 	$('#btnSubmit').click(function () {
@@ -31,7 +36,26 @@ $(document).ready(function () {
 		});
 
 	});
+
+	// Updates
+	var updateLoop = setInterval(function () {
+		if (todosList) {
+			oldList = todosList;
+			if (todosList === oldList && sleepycounter < 6) {
+				// Every five minutes, update the table order and the priorities
+				console.log("Just updating the numbers");
+				updateTable(todosList)
+				sleepycounter++;
+			} else {
+				// Every half hour, poll the server
+				console.log("Updating the list");
+				sleepycounter = 0;
+				GetTasks();
+			}
+		}
+	}, 300000);
 });
+
 
 function JSDateToExcelDate(inDate) {
 	var returnDateTime = 25569.0 + ((inDate.getTime() - (inDate.getTimezoneOffset() * 60 * 1000)) / (1000 * 60 * 60 * 24));
@@ -47,32 +71,44 @@ function GetTasks() {
 		},
 		success: function (result) {
 			if (result) {
-				console.log(result);
-				var len = result.length;
-				var txt = "";
-				var priority;
-				var rightNow = new Date();
-				if (len > 0) {
-					for (var i = 0; i < len; i++) {
-						if (result[i].coursecode && result[i].task && result[i].deadline && result[i].weight) {
-							result[i].deadline = new Date(result[i].deadline);
-							priority = (result[i].weight / (JSDateToExcelDate(result[i].deadline) - JSDateToExcelDate(rightNow))) * 100;
-							priority = parseFloat(priority).toFixed(2);
-							if (priority < 0) {
-								continue;
-							}
-							txt += '<tr><td><i class="fa fa-times remove" onclick="deleteItem(\'' + result[i]._id + '\')"></i></td><td>' + result[i].coursecode + '</td><td>' + result[i].task + '</td><td>' + result[i].deadline + '</td><td>' + result[i].weight + '</td><td class="priority">' + priority + '</td></tr>';
-						}
-					}
-					if (txt != "") {
-						$("#tasksTable").find("tr:gt(0)").remove();
-						$("#tasksTable").append(txt);
-						sortTable();
-					}
-				}
+				todosList = result;
+				updateTable(todosList);
 			}
+		},
+		timeout: function () {
+			console.log("Timeout");
+			updateTable(todosList);
+		},
+		error: function (error) {
+			console.log("Error", error);
+			updateTable(todosList);
 		}
 	});
+}
+
+function updateTable(result) {
+	var len = result.length;
+	var txt = "";
+	var priority;
+	var rightNow = new Date();
+	if (len > 0) {
+		for (var i = 0; i < len; i++) {
+			if (result[i].coursecode && result[i].task && result[i].deadline && result[i].weight) {
+				result[i].deadline = new Date(result[i].deadline);
+				priority = (result[i].weight / (JSDateToExcelDate(result[i].deadline) - JSDateToExcelDate(rightNow))) * 100;
+				priority = parseFloat(priority).toFixed(2);
+				if (priority < 0) {
+					continue;
+				}
+				txt += '<tr><td><i class="fa fa-times remove" onclick="deleteItem(\'' + result[i]._id + '\')"></i></td><td>' + result[i].coursecode + '</td><td>' + result[i].task + '</td><td>' + result[i].deadline + '</td><td>' + result[i].weight + '</td><td class="priority">' + priority + '</td></tr>';
+			}
+		}
+		if (txt != "") {
+			$("#tasksTable").find("tr:gt(0)").remove();
+			$("#tasksTable").append(txt);
+			sortTable();
+		}
+	}
 }
 
 function sortTable() {
