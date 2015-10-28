b474d85ecf5f53ae51a7b82435bdab173c206237
David Schlachter <david+gitlab@schlachter.ca>
2015-10-28 12:54:15 -0400

    Restore accidentally deleted main.js

---
+++ public/javascripts/main.js
@@ -0,0 +1,306 @@
+
+"use strict";
+
+var todosList;
+var oldList;
+var sleepycounter = 0;
+var justChanged;
+var fading;
+var backgroundImage, backgroundOpacity;
+
+$(document).ready(function () {
+
+	if ($('#inputDeadline').length) {
+		$.datetimepicker.setLocale('en');
+		$('#inputDeadline').datetimepicker();
+	}
+
+	if ($("#tasksTable").length) {
+		getTasks();
+		sortTable();
+	}
+
+
+	getPrefs();
+
+	$('#btnSubmit').click(function () {
+		justChanged = $('#inputCourseCode').val() + " " + $('#inputTask').val();
+		addTask($('#inputCourseCode').val(), $('#inputTask').val(), $('#inputDeadline').val(), $('#inputWeight').val() / 100);
+	});
+
+	$('#prefslink').click(function () {
+		$('#prefs').show();
+		$('#bigbkgdiv').show();
+		$('#inputBkgURL').val(backgroundImage);
+		$('#inputOpacity').val(backgroundOpacity);
+	});
+
+	$('#btnPrefSubmit').click(function () {
+		$('#prefs').hide();
+		$('#bigbkgdiv').hide();
+		$.ajax({
+			url: 'setPrefs',
+			type: 'POST',
+			data: {
+				BkgURL: $('#inputBkgURL').val(),
+				inputOpacity: $('#inputOpacity').val()
+			},
+			success: function (result) {
+				if (result) {
+					getPrefs();
+				}
+			},
+			timeout: function () {
+				console.log("setPrefs: timeout");
+			},
+			error: function (error) {
+				console.log("setPrefs: error", error);
+			}
+		});
+	});
+
+	// Updates
+	var updateLoop = setInterval(function () {
+		if (todosList) {
+			oldList = todosList;
+			if (todosList === oldList && sleepycounter < 6) {
+				// Every five minutes, update the table order and the priorities
+				console.log("Just updating the numbers");
+				updateTables(todosList);
+				sleepycounter++;
+			} else {
+				// Every half hour, poll the server
+				console.log("Updating the list");
+				sleepycounter = 0;
+				getTasks();
+				getPrefs();
+			}
+		}
+	}, 300000);
+});
+
+
+function jsDateToExcelDate(inDate) {
+	var returnDateTime = 25569.0 + ((inDate.getTime() - (inDate.getTimezoneOffset() * 60 * 1000)) / (1000 * 60 * 60 * 24));
+	return returnDateTime.toString().substr(0, 20);
+}
+
+function getTasks() {
+	$.ajax({
+		url: 'getTasks',
+		type: 'POST',
+		data: {
+			gettasks: "get tasks"
+		},
+		success: function (result) {
+			if (result) {
+				todosList = result;
+				updateTables(todosList);
+			}
+		},
+		timeout: function () {
+			console.log("Timeout");
+			updateTables(todosList);
+		},
+		error: function (error) {
+			console.log("Error", error);
+			updateTables(todosList);
+		}
+	});
+}
+
+function getPrefs() {
+	$.ajax({
+		url: 'getPrefs',
+		type: 'POST',
+		data: {
+			gettasks: "get prefs"
+		},
+		success: function (result) {
+			if (result) {
+				backgroundImage = result[0].backgroundURL;
+				backgroundOpacity = result[0].backgroundOpacity;
+				$('body').css('background', 'url(' + backgroundImage + ') no-repeat left top');
+				$('body').css('background-size', 'cover');
+				$('#content').css('background-color', 'rgba(255, 255, 255, ' + backgroundOpacity + ')');
+				console.log(result);
+			}
+		},
+		timeout: function () {
+			console.log("getPrefs: timeout");
+		},
+		error: function (error) {
+			console.log("getPrefs: error", error);
+		}
+	});
+}
+
+function updateTables(result) {
+	var len = result.length;
+	var currentText, completedText, priority, style, weight, dueDate, isCompleted, completedButton, priorityColumn, string;
+	var rightNow = new Date();
+	var rightNowPlusSeven = rightNow.addDays(7);
+	var rightNowPlusFourteen = rightNow.addDays(14);
+	if (len > 0) {
+		for (var i = 0; i < len; i++) {
+			if (result[i].coursecode && result[i].task && result[i].deadline && result[i].weight) {
+				result[i].deadline = new Date(result[i].deadline);
+				dueDate = new Date(result[i].deadline);
+				weight = parseFloat(result[i].weight * 100).toFixed(1);
+				priority = (result[i].weight / (jsDateToExcelDate(result[i].deadline) - jsDateToExcelDate(rightNow))) * 100;
+				priority = parseFloat(priority).toFixed(2);
+				if (priority < 0 || result[i].completed) {
+					isCompleted = true;
+				} else {
+					isCompleted = false;
+				}
+				if (isCompleted === false) {
+					completedButton = '<i title="Mark completed" class="fa fa-check complete" onclick="completeItem(\'' + result[i]._id + '\')"></i>';
+					priorityColumn = '<td class="priority">' + priority + '</td>'
+					if (dueDate && dueDate < rightNowPlusSeven) {
+						style = ' style="background-color: red;"';
+					} else if (dueDate && dueDate < rightNowPlusFourteen) {
+						style = ' style="background-color: yellow;"';
+					} else {
+						style = "";
+					}
+				} else { // If the task is completed
+					completedButton = '';
+					priorityColumn = '';
+					style = "";
+				}
+				string = '<tr><td id="' + result[i]._id + '">' + completedButton + '<i title="Delete task" class="fa fa-times remove" onclick="deleteItem(\'' + result[i]._id + '\')"></i></td><td>' + result[i].coursecode + '</td><td>' + result[i].task + '</td><td class="dateTD"' + style + ' name="' + dueDate.toString() + '">' + moment(dueDate).format("ddd") + '</td><td class="dateTD"' + style + '>' + moment(dueDate).format("D") + '</td><td class="dateTD"' + style + '>' + moment(dueDate).format("MMM,") + '</td><td class="dateTD time"' + style + '>' + moment(dueDate).format("h:mm A") + '</td><td class="weight" name="' + result[i].weight + '">' + weight + '%</td>' + priorityColumn + '</tr>';
+
+				if (isCompleted === false) {
+					currentText += string;
+				} else {
+					completedText += string;
+				}
+			}
+		}
+		if (currentText !== "") {
+			$("#tasksTable").find("tr:gt(0)").remove();
+			$("#tasksTable").append(currentText);
+			sortTable();
+		}
+		if (completedText !== "") {
+			$("#completedTable").find("tr:gt(0)").remove();
+			$("#completedTable").append(completedText);
+		}
+	}
+}
+
+function sortTable() {
+	var tbl = document.getElementById("tasksTable").tBodies[0];
+	var store = [],
+		len;
+	for (var i = 0, len = tbl.rows.length; i < len; i++) {
+		var row = tbl.rows[i];
+		var sortnr = parseFloat(row.cells[8].textContent || row.cells[8].innerText);
+		if (!isNaN(sortnr)) store.push([sortnr, row]);
+	}
+	store.sort(function (y, x) {
+		return x[0] - y[0];
+	});
+	for (var j = 0, len = store.length; j < len; j++) {
+		tbl.appendChild(store[j][1]);
+	}
+	store = null;
+	var tableWidth = $("#tasksTable").width;
+	$("#content").width(tableWidth);
+}
+
+function addTask(coursecode, task, deadline, weight) {
+	$.ajax({
+		url: 'addTask',
+		type: 'POST',
+		data: {
+			'coursecode': coursecode,
+			'task': task,
+			'deadline': deadline,
+			'weight': weight
+		},
+		success: function (result) {
+			getTasks();
+			showToast("Task " + justChanged + " added.");
+		},
+		timeout: function () {
+			showToast("Task could not be added. Server did not respond.");
+		},
+		error: function (error) {
+			console.log(error);
+			showToast("Task could not be added. Server gave an error.");
+		}
+	});
+}
+
+function deleteItem(id) {
+	var t = $("#" + id);
+	var undo = ' <a href="#" onclick="addTask(\'' + t.next().text() + '\',\'' + t.next().next().text() + '\',\'' + t.next().next().next().attr("name") + '\',\'' + t.next().next().next().next().next().next().next().attr("name") + '\');return false;">Undo</a>';
+	justChanged = t.next().text() + " " + t.next().next().text();
+	$.ajax({
+		url: 'deleteTask',
+		type: 'POST',
+		data: {
+			'delID': id
+		},
+		success: function (result) {
+			if (result) {
+				getTasks();
+				showToast("Task " + justChanged + " deleted.", undo);
+			}
+		},
+		timeout: function () {
+			showToast("Task could not be deleted. Server did not respond.");
+		},
+		error: function (error) {
+			console.log(error);
+			showToast("Task could not be deleted. Server gave an error.");
+		}
+	});
+}
+
+function completeItem(id) {
+	$.ajax({
+		url: 'completeTask',
+		type: 'POST',
+		data: {
+			'complID': id
+		},
+		success: function (result) {
+			if (result) {
+				getTasks();
+			}
+		},
+		timeout: function () {
+			showToast("Task could not be marked as complete. Server did not respond.");
+		},
+		error: function (error) {
+			console.log(error);
+			showToast("Task could not be marked as complete. Server gave an error.");
+		}
+	});
+}
+
+Date.prototype.addDays = function (days) {
+	var dat = new Date(this.valueOf());
+	dat.setDate(dat.getDate() + days);
+	return dat;
+};
+
+var fadeToast = function () {
+	$("#toast").empty();
+	$("#toast").hide();
+};
+
+function showToast(text, action) {
+	clearTimeout(fading);
+	$("#toast").text(text);
+	if (action) {
+		var oldHTML = $("#toast").html();
+		$("#toast").html(oldHTML + action);
+	}
+	$("#toast").show();
+	$("#toast").css('background-color', 'pink');
+	fading = setTimeout(fadeToast, 8000);
+}
